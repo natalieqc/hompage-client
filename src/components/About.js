c475201c752bab71ab93f6a2a84626febf64145c
qc <nataliechenqh@gmail.com>
2020-07-13 22:31:01 +0200

    add home and about components
    add home and about components

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const about = () => {
+  return (
+    <div>
+      <h4> about-page under construction </h4>
+    </div>
+  )
+}
+
+export default about;
---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const home = () => {
+  return (
+    <div>
+      <h4> homepage under construction </h4>
+    </div>
+  )
+}
+
+export default home;
