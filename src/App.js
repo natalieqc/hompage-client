719b992ec7b21c031c0dfde1656635c4cfc33869
qc <nataliechenqh@gmail.com>
2020-07-13 23:14:17 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import {HashRouter, Route, Switch} from 'react-router-dom';
 import Home from './components/Home.js';
 import About from './components/About.js';
 
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter basename="/client">
       <div>
         <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/about" component={About} />
         </Switch>
       </div>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
